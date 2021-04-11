4629819233462cb5581798390eda6af968cbc9b7
Anita <ana@codewithdragos.com>
2021-04-11 13:55:53 +0200

    exercises

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+    //inicializa la clase
+  constructor(props){
+    super(props);
+    //si vas a usar this.state tienes que darle un estado inicial
+    this.state = {date: new Date()}
+  }
+  render(){
+      //esto es lo que te devuelve esta funcion
+    return <div></div>;
+  }
+}
+//esto lo transforma en html y lo mete en app
+ReactDOM.render(<Clock />, document.getElementById('app'));
